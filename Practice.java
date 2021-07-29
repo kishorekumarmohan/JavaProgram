bea1d8f74cffd0c5b36e04e576414fe24fa9e74f
Kishore Kumar Mohan <kishorekumarm@gmail.com>
2021-07-29 13:52:02 -0700

    Update Practice.java

--- Practice.java
+++ Practice.java
@@ -10,7 +10,7 @@ public class Practice {
 ///uuuuu
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//jhgsadfjhagdfjhg
 		Scanner sc= new Scanner(System.in);
 		
 		System.out.println("Enter the string");
